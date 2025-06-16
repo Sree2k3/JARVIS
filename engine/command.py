3508c0a2b68e5704c8fc905ba93590662b7b99ba
Sreekant <1408sreekant@gmail.com>
2025-06-16 11:00:05 +0530

    tried for speech to text but did'nt work

--- engine/command.py
+++ engine/command.py
@@ -1,14 +1,42 @@
 import pyttsx3
+import speech_recognition as sr
+import eel
 
 
 def speak(text):
-    engine = pyttsx3.init('sapi5')
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[0].id) 
-    engine.setProperty('rate', 150) 
+    engine = pyttsx3.init("sapi5")
+    voices = engine.getProperty("voices")
+    engine.setProperty("voice", voices[0].id)
+    engine.setProperty("rate", 150)
     print(voices)
     engine.say(text)
     engine.runAndWait()
-    
-    
-speak("I love India")
+
+
+def takecommand():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("listening....")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+
+        audio = r.listen(source, 10, 6)
+
+    try:
+        print("recognizing")
+        query = r.recognize_google(audio, language="en-in")
+        print(f"user said: {query}")
+
+    except Exception as e:
+        return ""
+
+    return query.lower()
+
+
+text = takecommand()
+
+if text:
+    speak(text)
+else:
+    speak("I didn't catch that. Please try again.")
