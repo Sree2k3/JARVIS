a299fcc5d89e0feb9f1d7a9dcf01ad2e6b5bf4b0
Sreekant <1408sreekant@gmail.com>
2025-06-17 23:24:38 +0530

    voice recog to frnt end

--- engine/command.py
+++ engine/command.py
@@ -1,6 +1,3 @@
-
-
-
 import pyttsx3
 import speech_recognition as sr
 import eel
@@ -28,11 +25,12 @@ def speak(text):
         print("TTS Error:", e)
         print("Try checking your audio output settings or use an alternative TTS library.")
 
-
+@eel.expose
 def takecommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
+        eel.DisplayMessage("Listening...")
         r.pause_threshold = 1
         r.adjust_for_ambient_noise(source)
 
@@ -44,46 +42,13 @@ def takecommand():
 
         try:
             print("Recognizing...")
+            eel.DisplayMessage("Recognizing...")
             query = r.recognize_google(audio, language="en-in")
             print(f"User said: {query}")
-            return query.lower()
-
-        except Exception as e:
-            return ""
-
-
-text = takecommand()
-speak(text)
-
-
-
-"""def speak(text):
-    engine = pyttsx3.init("sapi5")
-    voices = engine.getProperty("voices")
-    engine.setProperty("voice", voices[0].id)
-    engine.setProperty("rate", 174)
-    print(voices)
-    engine.say(text)
-    engine.runAndWait()
-
-
-def takecommand():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Listening...")
-        r.pause_threshold = 1
-        r.adjust_for_ambient_noise(source)
-        audio = r.listen(source, 10, 6)
-        try:
-            print("Recognizing...")
-            query = r.recognize_google(audio, language="en-in")
-            print(f"User said: {query}\n")
-            eel.display(query)  # Call the eel function to display the text
+            eel.DisplayMessage(query)
+            speak(query)
+            eel.ShowHood()
         except Exception as e:
             return ""
 
         return query.lower()
-
-
-text = takecommand()
-speak(text)"""
